4775286775e4eda7a0f2ce55cdf827dc46c6d0cc
zibi666 <1925328075@qq.com>
2026-01-15 13:16:45 +0800

    修改睡眠音乐播放路径

--- main/bsp/sd_audio/audio_sdcard.h
+++ main/bsp/sd_audio/audio_sdcard.h
@@ -7,6 +7,15 @@
 #define AUDIO_MUSIC_DIR        AUDIO_SD_MOUNT_POINT "/MUSIC"
 #define AUDIO_MUSIC_DIR_FAT    "0:/MUSIC"
 
+/* 唤醒音乐和睡眠音乐路径 */
+#define AUDIO_WAKE_MUSIC_DIR     AUDIO_SD_MOUNT_POINT "/MUSIC/wake"
+#define AUDIO_WAKE_MUSIC_DIR_FAT "0:/MUSIC/wake"
+#define AUDIO_SLEEP_MUSIC_DIR     AUDIO_SD_MOUNT_POINT "/MUSIC/sleep"
+#define AUDIO_SLEEP_MUSIC_DIR_FAT "0:/MUSIC/sleep"
+
+/* 唤醒音乐总数 */
+#define AUDIO_WAKE_MUSIC_COUNT 177
+
 esp_err_t audio_sdcard_mount(void);
 void audio_sdcard_unmount(void);
 bool audio_sdcard_is_mounted(void);
